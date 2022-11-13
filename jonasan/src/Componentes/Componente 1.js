aa5d25862fc316270e9d7579c4ac4e5ca8a00027
JonasT29 <jonastredici@gmail.com>
2022-11-13 17:26:05 -0300

    agregar app

---
+++ jonasan/src/Componentes/Componente 1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Componente 1 = () => {
+  return (
+    <div>
+        <h1> Hola me llamo Jonás</h1>
+        
+      
+    </div>
+  )
+}
+
+export default Componente 1
